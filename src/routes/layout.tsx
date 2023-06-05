9f8ba2a7fa706223c33dc544ccc30bc11b74e13a
saboooor <saboor547@gmail.com>
2023-06-05 01:08:47 -0600

    the fuck is clarity

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,7 +1,18 @@
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot, useVisibleTask$ } from '@builder.io/qwik';
 import Nav from '../components/Nav';
 
 export default component$(() => {
+  useVisibleTask$(() => {
+    (window as any).clarity = (window as any).clarity || function(...args: any) {
+      ((window as any).clarity.q = (window as any).clarity.q || []).push(args);
+    };
+    const t = document.createElement('script');
+    t.async = true;
+    t.src = 'https://www.clarity.ms/tag/hf0fh28kj3';
+    const y = document.getElementsByTagName('script')[0];
+    y.parentNode!.insertBefore(t, y);
+  }, { strategy: 'document-idle' });
+
   return (
     <main>
       <Nav />
